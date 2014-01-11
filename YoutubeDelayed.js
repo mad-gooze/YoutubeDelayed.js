670a979abbcbc8e955fea7731316ab954c93a8e7
MAD-GooZe <mad.gooze@gmail.com>
2014-01-12 02:31:03 +0400

    Initial commit

---
+++ YoutubeDelayed.js
@@ -0,0 +1,24 @@
+var YoutubeDelayed = {
+    initPlayer: function(node){
+        //initialise the player in specified node
+        var videoId = node.getAttribute('data-videoid');
+        node.style.backgroundImage = "url(http://img.youtube.com/vi/" + videoId + "/0.jpg)";
+        node.innerHTML = '<a class="YoutubeDelayedPlay" href="http://www.youtube.com/watch?v=' + videoId +'" target="_blank"></a>'
+        node.firstChild.onclick = YoutubeDelayed.loadPlayer;
+    },
+    init: function(){
+        //initialise the players
+        var players = document.getElementsByClassName("YoutubeDelayed");
+        for (var i = 0; i < players.length; i++){
+            YoutubeDelayed.initPlayer(players[i]);
+        }
+    },
+    loadPlayer: function(e){
+        //load player when user clicked play button
+        e.preventDefault();
+        this.parentNode.innerHTML = '<iframe class="YoutubeDelayedPlayer" src="http://www.youtube.com/embed/' + this.parentNode.getAttribute('data-videoid') + '?autoplay=1" frameborder="0" allowfullscreen></iframe>';
+        //console.log('<iframe class="YoutubeDelayedPlayer" src="http://www.youtube.com/embed/' + this.parentNode.getAttribute('data-videoid') + '?autoplay=1" frameborder="0" allowfullscreen></iframe>')
+    }
+}
+//set cross-browser onload event
+window[addEventListener ? 'addEventListener' : 'attachEvent'](addEventListener ? 'load' : 'onload', YoutubeDelayed.init);
